dfc7180d9c1c6fa32effd8e02c83056f7bf5b169
Natalie Baker <nathelene76@gmail.com>
2023-07-25 16:53:42 -0600

    fix cypress errors on home

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -29,35 +29,17 @@ describe('Home Page', () => {
   })
   
   it('user sees a Form with an input field on the home page', () => {
-    expect(true).to.equal(true)
     cy.get('nav').should('be.visible')
-    .get('h1').contains('Weather App')
-    .get('button').contains('View Saved Locations')
-    .get('h2').contains('Select Your Location')
-    .get('input').should("be.visible")
+      .get('.title').contains("h1",'Solar Sense')
+      .get('.view-saved-button')
+      .get('h2').contains('Select Your Location')
+      .get('input').should("be.visible")
   });
   
   it('user sees the input, types a City, and sees the info card', () => {
     expect(true).to.equal(true)
     cy.get('input').type('Denver')
-  //   .get('.card--link').last().contains('p', 'I Still Believe')
-  //   .get('.rating').first().contains('span', '66%')
-  //   .get('.poster').should('be.visible')
-  //   .get('.poster').should('have.attr', 'style').should('include',`background-image: url("https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg")`)
-  //   .get('.tomato').should('be.visible')
-  //   .get('.tomato').first().should('have.attr', 'src').should('include', '/static/media/tomato.b8416d23ec32ff330201.png')
-  //   .get('.tomato').last().should('have.attr', 'src').should('include', '/static/media/green-slime.920d7e5db149537ea048.png')
+      .get(".card").contains("h1", "Denver, Colorado, United States")
   });
-  
-  // it('as a user I should be able to click on a card and the url should change to a new id', () => {
-  //   cy.get('.card--link').first().click()
-  //   cy.url().should('include', '694919')
-  // });
-  
-  
-  // it('as a user I should be able to click on the home button and the changes to the url should be reflected', () => {
-  //   cy.get('.rt-logo').click()
-  //   .url().should('eq', 'http://localhost:3000/')
-  // });
 
 });  

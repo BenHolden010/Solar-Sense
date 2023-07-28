7aee33ca108dda71bf0259aa8f1785a441b4e29d
Denzel Turambi <dturambi26@gmail.com>
2023-07-28 11:55:35 -0600

    refactor: Refactor styling/positioning for buttons

--- src/Components/Focus.tsx
+++ src/Components/Focus.tsx
@@ -17,10 +17,12 @@ const FocusCard = (props: Props) => {
   return (
     <section className={`focus-section`}>
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-      <NavLink to="/">  <button className="back-button"><span className="material-symbols-outlined">
-        arrow_back</span>
+      <div className="focus-section-nav">
+      <NavLink to="/">  
+      <button className="back-button"><span className="material-symbols-outlined">arrow_back</span>
       </button>
       </NavLink>
+      </div>
       <div className='focus-card'>
         <h1>{props.locationName}, {props.locationRegion}, {props.locationCountry === "United States of America" ? "United States" : props.locationCountry} </h1>
         <p className="focus-temp">{props.temp} °F</p>
--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -6,6 +6,7 @@ export default function Nav() {
     return (
         <nav className="nav">
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+            <div className="invisible-div"></div>
             <h1 className="title">Solar Sense</h1>
             <NavLink to='/saved-locations'>
                 <button className="view-saved-button"><span className="material-symbols-outlined">bookmarks</span></button>
